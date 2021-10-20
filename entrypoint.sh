024c220adde57701765630c30b53cb4f4096b1ed
hernandito <archi@inch.com>
2021-10-20 17:52:58 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /mnt/cron.conf ]; then
 #set time to 12:30AM run
-    echo "30 * * * * /config/tinyMediaManagerCMD.sh -updateMovies -updateTv -scrapeNew > /mnt/cron.conf
+    echo "0 */2 * * * /config/tinyMediaManagerCMD.sh -updateMovies -updateTv -scrapeNew > /mnt/cron.conf
 fi
 chmod -R 777 /config/
 chmod 600 /mnt/cron.conf
